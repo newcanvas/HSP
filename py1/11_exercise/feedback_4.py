5fa7b30d0b184832fd3dc3394fe36b1cc4a89201
Anna Akh <chtopodelat@gmail.com>
2025-08-02 23:58:28 +0500

    added feedback_4.py

---
+++ py1/11_exercise/feedback_4.py
@@ -0,0 +1,15 @@
+def open_and_sum(inf_list):
+    summ = 0
+    for file in inf_list:
+        try:
+            current_file = open(file, 'rt')
+            for line in current_file:
+                summ+=int(line)
+        except ValueError:
+            print("Внутри файлов что-то кроме чисел")
+        except Exception:
+            print("Один из файлов поврежден, нужно разбираться")
+        finally:
+            current_file.close()
+            #print(summ)
+    return summ

e57967e8a59093365086d2e84d7b76ed48014018
Anna Akh <chtopodelat@gmail.com>
2025-08-04 13:33:09 +0500

    edited feedback_4.py

--- py1/11_exercise/feedback_4.py
+++ py1/11_exercise/feedback_4.py
@@ -1,14 +1,22 @@
-def open_and_sum(inf_list: list) -> int:
+def final(inf_list: list) -> list:
     summ = 0
     for file in inf_list:
-        try:
-            current_file = open(file, 'rt')
-            for line in current_file:
-                summ+=int(line)
-        except ValueError:
-            return "Внутри файлов что-то кроме чисел"
-        except Exception:
-            return "Один из файлов поврежден, нужно разбираться"
-        finally:
-            current_file.close()
-    return summ
+        current_file = file_sum(file)
+        if current_file[1] == 0:
+            summ += current_file[0]
+        else:
+            return [0, current_file[1]]
+    return [summ, 0]
+
+def file_sum(file):
+    try:
+        with open(file, 'rt') as files:
+            summ = 0
+            for i in range(3):
+                line = files.readline()
+                summ += int(line)
+            return [summ, 0]
+    except FileNotFoundError:
+        return [0, 2]
+    except Exception as ex:
+        return [0, 1]

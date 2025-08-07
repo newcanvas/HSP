d1e3a8276b18018da4b6245c2f2344b8b1d7578e
Anna Akh <chtopodelat@gmail.com>
2025-08-07 23:21:20 +0500

    edited feedback_6.py

--- py1/feedback_6.py
+++ py1/feedback_6.py
@@ -4,15 +4,18 @@ def FindValue(N):
     final = []
     keys = []
     dictionary = {}
-    
-    for key in range(100):
-        keys.append(randint(1,10))
-        dictionary[keys[key]] = 0
-    
-    for i in keys:
-        dictionary[i]+=1 
 
-    for i in dictionary:
-        if dictionary.get(i) == N:
-            final.append(i)
+    for key in range(100):
+        num = randint(1,10)
+        if num in keys:
+            dictionary[num] += 1
+            keys.append(num)
+            if dictionary[num] == N:
+                final.append(num)
+            if dictionary[num] > N:
+                final.remove(num)
+                dictionary[num] = -1000
+            continue
+        keys.append(num)
+        dictionary[num] = 1
     return final

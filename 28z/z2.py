49bd3a40c72e82422a90bc58339c62dafee51252
Anna Akh <chtopodelat@gmail.com>
2025-08-13 14:15:55 +0500

    edited z2.py

--- 28z/z2.py
+++ 28z/z2.py
@@ -1,8 +1,13 @@
 def odometer(oksana: list) -> int:
     total = 0
     for i in range(len(oksana)):
-        if i % 2 == 0:
-            total += oksana[i]
+        hours = 0
+        if i == 0:
+            hours = oksana[i+1]
+            total += oksana[i] * hours
+        elif i % 2 == 0:
+            hours = oksana[i+1] - oksana[i-1]
+            total += oksana[i] * hours
 
     return total
 

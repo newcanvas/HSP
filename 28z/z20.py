b844df152542faaeee3a273e1a0b53bb58fb4229
Anna Akh <chtopodelat@gmail.com>
2025-09-25 16:47:37 +0500

    edited z20.py

--- 28z/z20.py
+++ 28z/z20.py
@@ -1,43 +1,51 @@
-def BastShoe(command: str) -> str:
-    
-    global string_now, all_strings, undo
+STRING_NOW = ''
+ALL_STRINGS = ['']
+UNDO = []
 
-    if command[0] not in ['1','2','3','4','5']:
-        return string_now
-    
+def BastShoe(command: str) -> str:
+
+    global STRING_NOW, ALL_STRINGS, UNDO
+
     if command[0] == '1':
-        if len(undo) > 0:
-            undo = []
-            all_strings = [string_now]
-        string_now += command[2:]
-        all_strings.append(string_now)
-    
+        if len(UNDO) > 0:
+            UNDO = []
+            ALL_STRINGS = [STRING_NOW]
+        STRING_NOW += command[2:]
+        ALL_STRINGS.append(STRING_NOW)
+        return STRING_NOW
+
     if command[0] == '2':
-        if len(undo) > 0:
-            undo = []
-            all_strings = [string_now]
-        string_now = string_now[:-int(command[2:])]
-        all_strings.append(string_now)
-    
+        if len(UNDO) > 0:
+            UNDO = []
+            ALL_STRINGS = [STRING_NOW]
+        STRING_NOW = STRING_NOW[:-int(command[2:])]
+        ALL_STRINGS.append(STRING_NOW)
+        return STRING_NOW
+
     if command[0] == '3':
-        if int(command[2:]) > len(string_now)-1:
+        if int(command[2:]) > len(STRING_NOW)-1:
             return ''
-        string_now = string_now[int(command[2:])]
-    
+        return STRING_NOW[int(command[2:])]
+
     if command[0] == '4':
-        if len(all_strings) > 1:
-            all_strings.pop(-1)
-            string_now = all_strings[-1]
-            undo.append(string_now)
-            
+        if len(ALL_STRINGS) > 1:
+            if not UNDO:
+                UNDO.append(STRING_NOW)
+            elif UNDO[-1] != STRING_NOW:
+                UNDO.append(STRING_NOW)
+            ALL_STRINGS.pop()
+            STRING_NOW = ALL_STRINGS[-1]
+            if UNDO[-1] != STRING_NOW:
+                UNDO.append(STRING_NOW)
+        return STRING_NOW
 
     if command[0] == '5':
-        if len(undo) > 0:
-            undo.pop(-1)
-            string_now = undo[-1]
-            
-    
-string_now = ''
-all_strings = ['']
-undo = []
+        if len(UNDO) > 1:
+            UNDO.pop()
+            STRING_NOW = UNDO[-1]
+            if STRING_NOW != ALL_STRINGS[-1]:
+                ALL_STRINGS.append(STRING_NOW)
+        return STRING_NOW
+
+    return STRING_NOW
 

9aac77fb2b36245c60b2224a6c6b323a6f5c2949
Anna Akh <chtopodelat@gmail.com>
2025-10-05 22:18:05 +0500

    added z23.py and z23_tests.py

---
+++ 28z/z23.py
@@ -0,0 +1,42 @@
+def TreeOfLife(H: int, W: int, N: int, tree: list) -> list:
+
+    all_lists = []
+    for i in range(H):
+        all_lists.append(list(tree[i]))
+
+    for i in range(H):
+        for j in range(W):
+            if all_lists[i][j] == '.':
+                 all_lists[i][j] = 0
+            else:
+                all_lists[i][j] = 1
+
+    for year in range(N):
+        for a in range(H):
+            for b in range(W):
+                all_lists[a][b] += 1
+
+        current_year = all_lists.copy()
+
+        if year % 2 != 0:
+            for a in range(H):
+                for b in range(W):
+                    if current_year[a][b] > 2:
+                        all_lists[a][b] = 0
+                        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                            ni, nj = a + dx, b + dy
+                            if 0 <= ni < H and 0 <= nj < W:
+                                all_lists[ni][nj] = 0
+
+    final = []
+    for h in range(H):
+        for w in range(W):
+            if all_lists[h][w] == 0:
+                all_lists[h][w] = '.'
+            else:
+                all_lists[h][w] = '+'
+        new_string = "".join(all_lists[h])
+        final.append(new_string)
+                                
+    return final
+
---
+++ 28z/z23_tests.py
@@ -0,0 +1,19 @@
+import unittest
+from z23 import TreeOfLife
+
+class DivTests(unittest.TestCase):
+
+    def test_regression(self):
+        self.assertEqual(TreeOfLife(3,4, 1, [".+..","..+.",".+.."]), ["++++","++++","++++"])
+    
+    def test_regression2(self):
+        self.assertEqual(TreeOfLife(3,4, 2, [".+..","..+.",".+.."]), ["...+","+...","...+"])
+    
+    def test_regression3(self):
+        self.assertEqual(TreeOfLife(3,4, 3, [".+..","..+.",".+.."]), ["++++","++++","++++"])
+
+    def test_regression4(self):
+        self.assertEqual(TreeOfLife(3,4, 4, [".+..","..+.",".+.."]), [".+..","..+.",".+.."])
+
+if __name__ == '__main__':
+    unittest.main()

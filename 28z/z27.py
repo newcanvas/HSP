4c7817f13724040d36ee0570e2761cd4b95affeb
Anna Akh <chtopodelat@gmail.com>
2025-10-20 19:47:55 +0500

    added z27.py and z27_tests.py

---
+++ 28z/z27.py
@@ -0,0 +1,27 @@
+def Football(F: list, N: int) -> bool:
+    F1 = F.copy()
+
+    affected_range = set()
+
+    for i in range(N-1, 0, -1):
+        if F1[i] < F1[i - 1]:
+            j = i - 1
+            while j >= 0 and F1[i] < F1[j]:
+                j -= 1
+
+            swap_pos = j + 1
+            F1[i], F1[swap_pos] = F1[swap_pos], F1[i]
+
+            affected_range.add(i)
+            affected_range.add(swap_pos)
+
+            if len(affected_range) > 2:
+                min_pos = min(affected_range)
+                max_pos = max(affected_range)
+
+                for pos in range(min_pos, max_pos + 1):
+                    if pos not in affected_range:
+                        return False
+
+    return True
+
---
+++ 28z/z27_tests.py
@@ -0,0 +1,22 @@
+import unittest
+from z27 import Football
+
+class DivTests(unittest.TestCase):
+
+    def test_regression(self):
+        self.assertEqual(Football([1,3,2], 3), True)
+    
+    def test_regression2(self):
+        self.assertEqual(Football([1,7,5,3,9], 5), True)
+    
+    def test_regression3(self):
+        self.assertEqual(Football([3,2,1], 3), True)
+
+    def test_regression4(self):
+        self.assertEqual(Football([9,5,3,7,1], 5), False)
+
+    def test_regression5(self):
+        self.assertEqual(Football([1,4,3,2,5], 5), True)
+
+if __name__ == '__main__':
+    unittest.main()

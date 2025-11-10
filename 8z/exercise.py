dc0cefc3dc3be2a1c89164577cb0897233ebddda
Anna Akh <chtopodelat@gmail.com>
2025-11-11 00:00:47 +0500

    edited tenet()

--- 8z/exercise.py
+++ 8z/exercise.py
@@ -29,9 +29,11 @@ def summ_list_2(list1):
 
 # 4. проверка, является ли строка палиндромом;
 
-def tenet(string1):
-    if string1 == '' or len(string1) == 0:
+def tenet(string1, start = 0, end = -1):
+    if len(string1) <= 1:
         return True
-    if string1[0] != string1[-1]:
+    if start == len(string1) // 2:
+        return True
+    if string1[start] != string1[end]:
         return False
-    return tenet(string1[1:-1])
+    return tenet(string1, start+1, end-1)
